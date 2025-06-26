dd8a5f86abf0a49d7705ebdaeb0155759aac2320
Sergey <supermatrix2001@gmail.com>
2025-06-26 23:08:06 +0300

    First try. Add: auth, hash, users, wishes, wishlists, offers, guards

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      skipMissingProperties: true,
+    }),
+  );
+
+  await app.listen(port);
 }
 bootstrap();
